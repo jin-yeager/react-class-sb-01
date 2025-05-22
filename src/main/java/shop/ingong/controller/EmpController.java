e2d0717b5407fcc6952af68382b87bdebcffd682
JIN_DDIT <zanalyst01@gmail.com>
2025-05-22 14:34:55 +0900

    feat: git ignore 수정

--- src/main/java/shop/ingong/controller/EmpController.java
+++ src/main/java/shop/ingong/controller/EmpController.java
@@ -22,9 +22,10 @@ public class EmpController {
         return "emp_list";      // → emp_list.mustache
     }
 
-    /** 사원 상세 **/
+    /** 사원 상세 **///자동 바인딩 => model.addAttribute("vo", vo)
+    //public String detail(@ModelAttribute EmpVO vo, Model model)
     @GetMapping("/emp_detail.do")
-    public String detail(@ModelAttribute EmpVO vo, Model model) {
+    public String detail(EmpVO vo, Model model) {
         EmpVO result = daoEmp.select(vo);
         model.addAttribute("vo", result);
         return "emp_detail";
@@ -32,7 +33,7 @@ public class EmpController {
 
     /** 사원 수정 폼 **/
     @GetMapping("/emp_mod.do")
-    public String modForm(@ModelAttribute EmpVO vo, Model model) {
+    public String modForm(EmpVO vo, Model model) {
         EmpVO result = daoEmp.select(vo);
         model.addAttribute("vo", result);
         return "emp_mod";       // → emp_mod.mustache
@@ -40,7 +41,7 @@ public class EmpController {
 
     /** 사원 수정 처리 **/
     @PostMapping("/emp_mod_act.do")
-    public String modAct(@ModelAttribute EmpVO vo, Model model) {
+    public String modAct(EmpVO vo, Model model) {
         int cnt = daoEmp.update(vo);
         model.addAttribute("cnt", cnt);
         return "emp_mod_act";   // → emp_mod_act.mustache
@@ -48,7 +49,7 @@ public class EmpController {
 
     /** 사원 삭제 처리 **/
     @GetMapping("/emp_del_act.do")
-    public String delAct(@ModelAttribute EmpVO vo, Model model) {
+    public String delAct(EmpVO vo, Model model) {
         int cnt = daoEmp.delete(vo);
         model.addAttribute("cnt", cnt);
         return "emp_del_act";   // → emp_del_act.mustache
@@ -62,7 +63,7 @@ public class EmpController {
 
     /** 사원 등록 처리 **/
     @PostMapping("/emp_add_act.do")
-    public String addAct(@ModelAttribute EmpVO vo, Model model) {
+    public String addAct(EmpVO vo, Model model) {
         int cnt = daoEmp.insert(vo);
         model.addAttribute("cnt", cnt);
         return "emp_add_act";   // → emp_add_act.mustache
